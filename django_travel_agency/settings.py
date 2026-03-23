b32cc8b4ec997360bf2c83024b927f443bd002e7
Snehal Gavali <snehalgavali5959@gmail.com>
2026-03-23 12:19:39 +0530

    Secured environment variables

--- django_travel_agency/settings.py
+++ django_travel_agency/settings.py
@@ -20,7 +20,11 @@ load_dotenv(BASE_DIR / ".env")
 # ===============================
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-default-key")
 
+<<<<<<< HEAD
 DEBUG =False
+=======
+DEBUG = False
+>>>>>>> b7f0681 (Secured environment variables)
 
 ALLOWED_HOSTS = ['.onrender.com']  # change in production
 
