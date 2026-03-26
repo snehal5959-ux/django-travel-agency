1ac0ff3bf8828d2761bde3a47702e0a30f902280
Snehal Gavali <snehalgavali5959@gmail.com>
2026-03-26 14:06:00 +0530

    Added auto superuser creation

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,22 @@
-from django.apps import AppConfig
 
+from django.apps import AppConfig
 
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        from django.db.utils import OperationalError, ProgrammingError
+
+        User = get_user_model()
+
+        try:
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    email="admin@gmail.com",
+                    password="admin123"
+                )
+        except (OperationalError, ProgrammingError):
+            pass
