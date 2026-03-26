720f5a92a29587d1f7fe24181d33c81898cbd9a7
Snehal Gavali <snehalgavali5959@gmail.com>
2026-03-26 13:53:59 +0530

    Configured PostgreSQL with Railway

--- django_travel_agency/settings.py
+++ django_travel_agency/settings.py
@@ -109,11 +109,12 @@ TEMPLATES = [
 # ===============================
 # DATABASE
 # ===============================
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 # ===============================
